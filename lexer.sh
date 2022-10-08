47092291fccdf85b5f04797e530d9e8a964f87e9
Stefan Welke <7465621+westenfalke@users.noreply.github.com>
2022-10-08 13:55:45 +0000

    Arguments and Options

--- lexer.sh
+++ lexer.sh
@@ -1,46 +1,70 @@
 #!/bin/bash
-
-function usage () {
-cat <<'HELPMSG'
-  usage $0 [--help] []
-
-HELPMSG
-}
-function help_wanted() {
-   [ "$#" -ge "1" ] && [ "$1" = '-h' ] || [ "$1" = '--help' ] || [ "$1" = "-?" ]
+stop() {
+  message=${1:-"OK (/)"}
+  exit_code=${2:-0}
+  printf >&2 '%s (%i)\n' "${message}" "${exit_code}"
+  exit "${exit_code}"
 }
 
-if help_wanted "$@"; then
-   usage
-   exit 0
+DEV_NULL="/dev/null"
+DEBUG=
+BUFFER=unset
+char=
+EXPRESSION=unset
+
+usage()
+{
+  echo "Usage: ${0} [ -g | --debug ] 
+                        [ -b | --buffer a string]
+                        [ -c | --char a singe char ] 
+                        [ -e | --expression (operation|string|number|symbol)]
+                        [filename]"
+  stop "Invalid argument" '22'
+}
+
+PARSED_ARGUMENTS=$(getopt -a -n ${0} -o g,b:c:e: --long debug,buffer:,char:,expression: -- "$@")
+VALID_ARGUMENTS=$?
+if [ "$VALID_ARGUMENTS" != "0" ]; then
+  usage
 fi
 
-die() {
-  printf >&2 '%s\n' "$1"
-  exit 1
-}
-from_source_file=$1
+eval set -- "$PARSED_ARGUMENTS"
+while :
+do
+  case "$1" in
+    -g | --debug)      DEBUG=1         ; shift   ;;
+    -b | --buffer)     BUFFER="$2"     ; shift 2 ;;
+    -c | --char)       CHAR="$2"       ; shift 2 ;;
+    -e | --expression) EXPRESSION="$2" ; shift 2 ;;
+    # -- means the end of the arguments; drop this, and break out of the while loop
+    --) shift; break ;;
+    # If invalid options were passed, then getopt should have reported an error,
+    # which we checked as VALID_ARGUMENTS when getopt was called...
+    *) echo "Famous Last Words - Unexpected option: $1 - this should not happen."
+       usage ;;
+  esac
+done
 
-##new_lines="\r\n"
-##stripp_source_code=$(tr --delete "x" < ${from_source_file:-/dev/null}) # just to fit in an array
-##IFS=";" ; declare -a source_code=(${stripp_source_code}) ; for element in "${source_code[@]}" ; do echo "$element" ; done # each line 
+INFLILE="${1:-${DEV_NULL}}"
+if [[ ${DEBUG} ]]
+then 
+   echo "PARSED_ARGUMENTS is $PARSED_ARGUMENTS"
+   echo "DEBUG   : ${DEBUG}"
+   echo "BUFFER  : ${BUFFER}"
+   echo "CHAR    : ${CHAR}"
+   echo "EXPRESSION   : ${EXPRESSION}"
+   echo "INFLILE : ${INFLILE}"
+   echo "Parameters remaining are: ${@}"
+fi
+
+from_source_file=${INFLILE} # play it save
  
 ORIG_IFS="$IFS"
 IFS=$'|\n;' ; read -d '' -a  source_code  < ${from_source_file} # don't stop on 'pipes', 'newlines' and 'semicolons'
 IFS="$ORIG_IFS"
-#echo '---';for element in "${source_code[@]}" ; do echo "$element" ; done ; echo '---' # put in a debug statement
+if [[ ${DEBUG} ]]; then echo '---';for element in "${source_code[@]}" ; do echo "$element" ; done ; echo '---'; fi #
 
-PREV_LINE=-1
-CURR_LINE=0
-NEXT_LINE=1
 
-echo ${source_code[$PREV_LINE]}
-echo ${source_code[$CURR_LINE]}
-echo ${source_code[$NEXT_LINE]}
-
-# example for non alpha (*[![:alpha:]]*)
-# ‘[a-dx-z]’ is equivalent to ‘[abcdxyz]’
-# alnum   alpha   ascii   blank   cntrl   digit   graph   lower print   punct   space   upper   word    xdigit
 CHAR_AMPERSAND='&'
 CHAR_EOF=EOF
 CHAR_NEWLINE='\n'
@@ -70,15 +94,18 @@ case "${c}" in
 #    [[:space:]]|"${CHAR_NEWLINE}) 
 #    [[:space:]]) 
         echo "pass:  # >>${c}<< is a [line breaking] whitspace"
+        return 0;
         ;;
 # special character        
-#     ${CHAR_PARAN_OPEN}|${CHAR_PARAN_CLOSE}|${CHAR_CURLY_OPEN}|${CHAR_PARAN_CLOSE}|${CHAR_COMMA}|${CHAR_SEMICOLON}|${CHAR_EQUAL_SIGN}|${CHAR_COLON})
-     [[:punct:]])
-        echo "yild(c, \"\") # >>${c}<< is a special character "
+     ${CHAR_PARAN_OPEN}|${CHAR_PARAN_CLOSE}|${CHAR_CURLY_OPEN}|${CHAR_PARAN_CLOSE}|${CHAR_COMMA}|${CHAR_SEMICOLON}|${CHAR_EQUAL_SIGN}|${CHAR_COLON})
+#     [[:punct:]])
+        echo "yield(c, \"\") # >>${c}<< is a special character "
+        exec ${0} "-c '${c}' -b '${source_code[$CURR_LINE]}'" 
         ;;
 # operator
      ${CHAR_PLUS_SIGN}|${CHAR_MINUS_SIGN}|\${CHAR_MUL}|${CHAR_DIV})
         echo "yield (\"operation\", c)' # operator >>${c}<<"
+        exec ${0} -g "-e 'operation' -c '${c}' -b '${source_code[$CURR_LINE]}'"
         ;;
 # string
      ${CHAR_QUOTE}|${CHAR_DOUBLE_QUOTE})
@@ -106,47 +133,11 @@ function PeekableString () {
     return
 }
 
-
-function test () {
-   lex ${CHAR_PARAN_OPEN}
-   lex ${CHAR_PARAN_CLOSE}
-   lex ${CHAR_CURLY_CLOSE}
-   lex ${CHAR_EQUAL_SIGN}
-   lex ${CHAR_SEMICOLON}
-   lex '.'
-   lex 9
-   lex "9"
-   lex "A"
-   lex "i"
-   lex '+'
-   lex '-'
-   lex '/'
-   lex '"'
-   lex "'"
-   lex '\t'
-   lex ${CHAR_NEWLINE}
-   lex ${CHAR_CARRIAGE_RETURN}
-   lex ' '
-   lex " "
-   return 0
-}
-
-test
-exit 0
-
 amount_of_lines=${#source_code[*]}
-for (( line_no=0; line_no<$(( $amount_of_lines )); line_no++ ))
-do 
-   line=${source_code[line_no]}
-   chars=${#line}
-   echo $chars' chars in line #'$line_no
-   for (( prev_char=-1, curr_char=0, next_char=1; curr_char<$(( $chars )); curr_char++, prev_char=curr_char-1, next_char=curr_char+1 ))
-   do
-      echo "#$line_no: $prev_char,$curr_char,$next_char;'${source_code[$line_no]}'"
-      echo "< prev_char '${source_code[$line_no]:$prev_char:1}'"
-      echo "| curr_char   '${source_code[$line_no]:$curr_char:1}'"
-      echo "> next_char    '${source_code[$line_no]:$next_char:1}'"
 
-   done
-done
-
+#source test.sh
+if [[ ! -z ${CHAR} ]] 
+then
+   lex "${CHAR}"
+fi
+stop
