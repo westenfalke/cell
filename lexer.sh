a4aebf32899424554aa183ec8d319ba1ef11cfde
Stefan Welke <7465621+westenfalke@users.noreply.github.com>
2022-10-09 01:43:47 +0000

    pattern matching via grep

--- lexer.sh
+++ lexer.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-set +x
 stop() {
   message=${1:-"OK (/)"}
   exit_code=${2:-0}
@@ -9,9 +8,9 @@ stop() {
 
 DEV_NULL="/dev/null"
 DEBUG=
-BUFFER=unset
-char=
-EXPRESSION=unset
+BUFFER=
+CHAR=
+EXPRESSION=
 
 usage()
 {
@@ -47,8 +46,7 @@ do
 done
 
 INFLILE="${1:-${DEV_NULL}}"
-if [[ ${DEBUG} ]]
-then 
+if [[ ${DEBUG} ]]; then 
    echo "PARSED_ARGUMENTS is $PARSED_ARGUMENTS"
    echo "DEBUG   : ${DEBUG}"
    echo "BUFFER  : ${BUFFER}"
@@ -86,49 +84,72 @@ CHAR_CURLY_CLOSE='}'
 CHAR_MUL='*'
 CHAR_DIV='/'
 CHAR_TAB='\t'
+PASS='0'
+
 
 function lex () {
-c="${1}"
+local c="${1}"
+echo lexi I "${c}"
+#if [[ "${CHAR_SEMICOLON}" == "${c}" ]]; then return ; fi
+echo lexi II
+local buff="${2}"
+if [[ -z "${buff}" ]]; then return ; fi
+local token=${3:-unset}
 case "${c}" in
-# Ignore (line breaking) white spacce
-    [[:space:]]|"${CHAR_NEWLINE}"|"${CHAR_TAB}"|"${CHAR_CARRIAGE_RETURN}") 
-#    [[:space:]]|"${CHAR_NEWLINE}) 
-#    [[:space:]]) 
-        echo "pass:  # >>${c}<< is a [line breaking] whitspace"
-        return 0;
-        ;;
-# special character        
-     ${CHAR_PARAN_OPEN}|${CHAR_PARAN_CLOSE}|${CHAR_CURLY_OPEN}|${CHAR_CURLY_CLOSE}|${CHAR_COMMA}|${CHAR_SEMICOLON}|${CHAR_EQUAL_SIGN}|${CHAR_COLON})
-#     [[:punct:]])
-        echo "yield(c, \"\") # >>${c}<< is a special character "
-        exec "${0}" -c "${c}" -b "${source_code[$CURR_LINE]}" -g 
-        ;;
-# operator
-     ${CHAR_PLUS_SIGN}|${CHAR_MINUS_SIGN}|\${CHAR_MUL}|${CHAR_DIV})
-        echo "yield (\"operation\", c)' # operator >>${c}<<"
-#        exec "${0}" -e "operation" -c "${c}" -b "${source_code[$CURR_LINE]}" -g
-        "${0}" -e "operation" -c "${c}" -b "${source_code[$CURR_LINE]}" -g
-        ;;
-# string
-     ${CHAR_QUOTE}|${CHAR_DOUBLE_QUOTE})
-        echo "yield (\"string\", _scan_string(c, chars)) # >>${c}<<"
-        ;;
-# number
-     [[:digit:]])
-        echo "yield (\"number\", _scan(c, chars, \"[.0-9]\"))# >>${c}<<"
-        ;;
-# symbole
-     [[:alpha:]])
-        echo 'yield ("symbol", _scan(c, chars, "[_a-zA-Z0-9]"))'
-        ;;
-# TAB not allowed
-     "${CHAR_TAB}")
-        echo "raise Exception(\"Tabs >>${c}<< are not allowed in Cell\")"
-        ;;
-    *)
-        echo "raise Exception(\"Unexpected character: >>${c}<<\")"
-        ;;
-    esac        
+   # Ignore (line breaking) white spacce
+   [[:space:]]|"${CHAR_NEWLINE}"|"${CHAR_TAB}"|"${CHAR_CARRIAGE_RETURN}") 
+      echo "pass:  # >>${c}<< is a [line breaking] whitspace"
+      echo "'${c}' '${buff}' '${token}'"
+      return ${PASS};
+   ;;
+   # special character        
+   ${CHAR_PARAN_OPEN}|${CHAR_PARAN_CLOSE}|${CHAR_CURLY_OPEN}|${CHAR_CURLY_CLOSE}|${CHAR_COMMA}|${CHAR_SEMICOLON}|${CHAR_EQUAL_SIGN}|${CHAR_COLON})
+      echo "yield(c, \"\") # >>${c}<< is a special character "
+      echo "'${c}' '${buff}' '${token}'"
+      if [[ "${buff:1:1}" == "${buff: -(${#buff}-1)}" ]]; then buff=${CHAR_SEMICOLON} ; fi
+      lex  ${buff:1:1} ${buff: -(${#buff}-1)}
+   ;;
+   # operator
+   ${CHAR_PLUS_SIGN}|${CHAR_MINUS_SIGN}|\${CHAR_MUL}|${CHAR_DIV})
+      echo "yield (\"operation\", c)' # operator >>${c}<<"
+      echo "'${c}' '${buff}' '${token}'"
+      if [[ "${buff:1:1}" == "${buff: -(${#buff}-1)}" ]]; then buff=${CHAR_SEMICOLON} ; fi
+      echo lex  ${buff:1:1} ${buff: -(${#buff}-1)} "operation"
+      lex  ${buff:1:1} ${buff: -(${#buff}-1)} "operation"
+   ;;
+   # string
+   ${CHAR_QUOTE}|${CHAR_DOUBLE_QUOTE})
+      echo "yield (\"string\", _scan_string(c, chars)) # >>${c}<<"
+      echo "'${c}' '${buff}' '${token}'"
+      if [[ "${buff:1:1}" == "${buff: -(${#buff}-1)}" ]]; then buff=${CHAR_SEMICOLON} ; fi
+      echo lex  ${buff:1:1} ${buff: -(${#buff}-1)} "string"
+      lex  ${buff:1:1} ${buff: -(${#buff}-1)} "string"
+   ;;
+   # number
+   [[:digit:]])
+      echo "yield (\"number\", _scan(c, chars, \"[.0-9]\"))# >>${c}<<"
+      echo "'${c}' '${buff}' '${token}'"
+   ;;
+   # symbole
+   [[:alpha:]])
+      echo 'yield ("symbol", _scan(c, chars, "[_a-zA-Z0-9]"))'
+      echo "'${c}' '${buff}' '${token}'"
+      symbol=$(grep -o -E "^(([[:alpha:]]+)([[:alnum:][_])*)*" <<< ${buff})
+      echo "symbol '${symbol}'"
+      if [[ ! (${#symbol} -lt ${#buff}) ]]; then buff=${CHAR_SEMICOLON} ; fi
+      echo "new buff '${buff: -(${#buff}-${#symbol})}'"
+      lex "${CHAR_SEMICOLON}" "${buff: -(${#buff}-${#symbol})}" "symbol"
+   ;;
+   # TAB not allowed
+   "${CHAR_TAB}")
+      echo "'${c}' '${buff}' '${token}'"
+      echo "raise Exception(\"Tabs >>${c}<< are not allowed in Cell\")"
+      ;;
+   *)
+      echo "'${c}' '${buff}' '${token}'"
+      echo "raise Exception(\"Unexpected character: >>${c}<<\")"
+   ;;
+esac
 }
 
 function PeekableString () {
@@ -143,19 +164,28 @@ amount_of_lines=${#source_code[*]}
 ###    lex "${CHAR}"
 ### fi
 
-   line=${source_code[line_no]}
+#   line=${source_code[line_no]}
+#   chars=${#line}
+if [[ ${BUFFER} ]]; then
+   line=${BUFFER}
    chars=${#line}
+###   for (( prev_char=-1, curr_char=0, next_char=1; curr_char<$(( $chars )); curr_char++, prev_char=curr_char-1, next_char=curr_char+1 ))
+###   do
+####      echo "#$line_no: $prev_char,$curr_char,$next_char;'${source_code[$CURR_LINE]}' char='${source_code[$line_no]:$curr_char:1}'"
+####      echo "$prev_char,$curr_char,$next_char;'${line}' char='${line:$curr_char:1}'"
+###
+####      lex "${source_code[$line_no]:$curr_char:1}"
+###
+###      lex "${line:$curr_char:1}"
+      echo START lex  "${line:0:1}" "${line}"
+      lex  "${line:0:1}" "${line}"
+stop
+###
+####      echo "< prev_char '${source_code[$line_no]:$prev_char:1}'"
+####      echo "| curr_char   '${source_code[$line_no]:$curr_char:1}'"
+####      echo "> next_char    '${source_code[$line_no]:$next_char:1}'"
 
-   for (( prev_char=-1, curr_char=0, next_char=1; curr_char<$(( $chars )); curr_char++, prev_char=curr_char-1, next_char=curr_char+1 ))
-   do
-      echo "#$line_no: $prev_char,$curr_char,$next_char;'${source_code[$CURR_LINE]}' char='${source_code[$line_no]:$curr_char:1}'"
-
-      lex "${source_code[$line_no]:$curr_char:1}"
-
-#      echo "< prev_char '${source_code[$line_no]:$prev_char:1}'"
-#      echo "| curr_char   '${source_code[$line_no]:$curr_char:1}'"
-#      echo "> next_char    '${source_code[$line_no]:$next_char:1}'"
-
-   done
+###   done
+fi
 
 stop
