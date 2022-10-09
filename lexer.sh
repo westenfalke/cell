496121875e4a040f9343bb209327f43fe9aa0497
Stefan Welke <7465621+westenfalke@users.noreply.github.com>
2022-10-09 20:52:14 +0000

    first pass

--- lexer.sh
+++ lexer.sh
@@ -109,7 +109,7 @@ case "${c}" in
       lex "${c}" "${buff}" "${token}"
    ;;
    # special character        
-   ${CHAR_PARAN_OPEN}|${CHAR_PARAN_CLOSE}|${CHAR_CURLY_OPEN}|${CHAR_CURLY_CLOSE}|${CHAR_COMMA}|${CHAR_SEMICOLON}|${CHAR_EQUAL_SIGN}|${CHAR_COLON})
+   "${CHAR_PARAN_OPEN}"|"${CHAR_PARAN_CLOSE}"|"${CHAR_CURLY_OPEN}"|"${CHAR_CURLY_CLOSE}"|"${CHAR_COMMA}"|"${CHAR_SEMICOLON}"|"${CHAR_EQUAL_SIGN}"|"${CHAR_COLON}")
       #echo "yield(c, '') # is a special character "
       token='special'
       echo "token='${token}'" 
@@ -121,11 +121,10 @@ case "${c}" in
       lex "${c}" "${buff}" "${token}"
    ;;
    # operator
-   ${CHAR_PLUS_SIGN}|${CHAR_MINUS_SIGN}|\${CHAR_MUL}|${CHAR_DIV})
+   "${CHAR_PLUS_SIGN}"|"${CHAR_MINUS_SIGN}"|"\${CHAR_MUL}"|"${CHAR_DIV}")
       #echo "yield ('operation', c) # operator >>${c}<< "
       token='operation'
       echo "token='${token}'" 
-      stop "paw '(${#buff}-1)'" '3'
       if [[ 2 -gt ${#buff} ]]; then buff=""; fi
       buff="${buff: -(${#buff}-1)}"
       c="${buff:0:1}"
@@ -134,7 +133,7 @@ case "${c}" in
       lex "${c}" "${buff}" "${token}"
    ;;
    # string
-   ${CHAR_QUOTE}|${CHAR_DOUBLE_QUOTE})
+   "${CHAR_QUOTE}"|"${CHAR_DOUBLE_QUOTE}")
       #echo "yield ('string', _scan_string(c, chars)) # >>${c}<<"
       token='string'
       echo "token='${token}'" 
@@ -153,12 +152,12 @@ case "${c}" in
       lex "${c}" "${buff}" "${token}"
    ;;
    # number
-   '[[:digit:]]')
+   [[:digit:]])
       #echo "yield ('number', _scan(c, chars, \"[.0-9]\"))# >>${c}<<"
       token='number'
       echo "token='${token}'" 
       number=$(grep -o -E "(^([[:digit:]])*([.][[:digit:]]){,1}([[:digit:]])*)" <<< ${buff})
-      if [[ ${number} == '' ]]; then stop "'.' is not a number" '1' ; fi
+      if [[ ${number} == '' ]]; then stop "'${c}' is not a number" '1' ; fi
       echo "number='${number}' [${#number}]"
       if [[ ${#number} -eq ${#buff} ]]; then buff=""; fi
       buff="${buff:${#number}:${#buff}}"
@@ -212,6 +211,11 @@ amount_of_lines=${#source_code[*]}
 if [[ ${BUFFER} ]]; then
    line=${BUFFER}
    chars=${#line}
+      lex  "${line:0:1}" "${line}"
+stop 'EOF'
+fi
+
+
 ###   for (( prev_char=-1, curr_char=0, next_char=1; curr_char<$(( $chars )); curr_char++, prev_char=curr_char-1, next_char=curr_char+1 ))
 ###   do
 ####      echo "#$line_no: $prev_char,$curr_char,$next_char;'${source_code[$CURR_LINE]}' char='${source_code[$line_no]:$curr_char:1}'"
@@ -220,14 +224,9 @@ if [[ ${BUFFER} ]]; then
 ####      lex "${source_code[$line_no]:$curr_char:1}"
 ###
 ###      lex "${line:$curr_char:1}"
-      lex  "${line:0:1}" "${line}"
-stop
 ###
 ####      echo "< prev_char '${source_code[$line_no]:$prev_char:1}'"
 ####      echo "| curr_char   '${source_code[$line_no]:$curr_char:1}'"
 ####      echo "> next_char    '${source_code[$line_no]:$next_char:1}'"
 
 ###   done
-fi
-
-stop
