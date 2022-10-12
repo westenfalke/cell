106b19583311bfe704adf3bd12b040352ff78264
Stefan Welke <7465621+westenfalke@users.noreply.github.com>
2022-10-12 03:30:26 +0200

    removed recursion (#2)
    * basic frame for a lexer
    
    * define finite states
    
    * line breaking whitspace
    
    * Arguments and Options
    
    * Arguments and Options
    
    * exec breaks loop, call required
    
    * pattern matching via grep
    
    * aplpha parsing
    
    * buffer and tooken
    
    * bump up
    
    * first pass
    
    * not supposed to be includet
    
    Signed-off-by: Stefan Welke <7465621+westenfalke@users.noreply.github.com>
    
    * nit suppused to be included
    
    Signed-off-by: Stefan Welke <7465621+westenfalke@users.noreply.github.com>
    
    * excluded dev container
    
    * debug, whitespace, numbes and stderr
    
    * line by line full path of a cell program
    
    * onsolete
    
    * obsolete
    
    Signed-off-by: Stefan Welke <7465621+westenfalke@users.noreply.github.com>
    
    * obsolete
    
    Signed-off-by: Stefan Welke <7465621+westenfalke@users.noreply.github.com>
    
    * obsolete
    
    Signed-off-by: Stefan Welke <7465621+westenfalke@users.noreply.github.com>
    
    * obsolete
    
    Signed-off-by: Stefan Welke <7465621+westenfalke@users.noreply.github.com>
    
    * oobsolete
    
    Signed-off-by: Stefan Welke <7465621+westenfalke@users.noreply.github.com>
    
    * prepared to remove recursion
    
    * removed recursion
    
    * shaved another 6 lines of fluff
    
    * removed global parameter variable names
    
    Signed-off-by: Stefan Welke <7465621+westenfalke@users.noreply.github.com>

--- lexer.sh
+++ lexer.sh
@@ -1,20 +1,42 @@
 #!/bin/bash
 exec 3>&1 1>&2
-stop() {
-   message=${1:-"OK (/)"}
-   exit_code=${2:-0}
+set -o errexit
+set -o nounset
 
-   if [[ ${exit_code} -gt 0 ]] || [[ ${DEBUG} ]]; then 
+declare -r _EMPTY_TOKEN_=''
+declare -r _SYMBOL_TOKEN_='symbol'
+declare -r _STRING_TOKEN_='string'
+declare -r _NUMBER_TOKEN_='number'
+declare -r _OPERATION_TOKEN_='operation'
+declare -r _NO_VALUE_=''
+declare -r _AMPERSAND_='&'
+declare -r _NEWLINE_='\n'
+declare -r _CARRIAGE_RETURN_='\r'
+declare -r _CHAR_PIPE_='|'
+declare -r _SEMICOLON_=';'
+declare -r _COLON_=':'
+declare -r _PLUS_SIGN_='+'
+declare -r _MINUS_SIGN_='-'
+declare -r _EQUAL_SIGN_='='
+declare -r _COMMA_=','_DIV_
+declare -r _PARAN_OPEN_='('
+declare -r _PARAN_CLOSE_=')'
+declare -r _QUOTE_="'"
+declare -r _DOUBLE_QUOTE_='"'
+declare -r _CURLY_OPEN_='{'
+declare -r _CURLY_CLOSE_='}'
+declare -r _MUL_='*'
+declare -r _DIV_='/'
+declare -r _TAB_='\t'
+
+stop() {
+   declare -r message=${1:-"OK (/)"}
+   declare -r -i exit_code=${2:-0}
+   if [[ ${exit_code} -gt 0 ]] || [[ ${OPT_DEBUG} ]]; then 
       printf '%s (%i)\n' "${message}" "${exit_code}"
    fi
    exit "${exit_code}"
 }
-
-DEV_NULL="/dev/null"
-DEBUG=
-BUFFER=
-CHAR=
-EXPRESSION=
 
 usage()
 {
@@ -31,14 +53,20 @@ if [ "$VALID_ARGUMENTS" != "0" ]; then
   usage
 fi
 
+declare OPT_DEBUG=
+declare ARG_BUFFER=
+declare ARG_CHAR=
+declare ARG_EXPRESSION=
+declare INFLILE=/dev/null
+
 eval set -- "$PARSED_ARGUMENTS"
 while :
 do
   case "$1" in
-    -g | --debug)      DEBUG=1         ; shift   ;;
-    -b | --buffer)     BUFFER="$2"     ; shift 2 ;;
-    -c | --char)       CHAR="$2"       ; shift 2 ;;
-    -e | --expression) EXPRESSION="$3" ; shift 2 ;;
+    -g | --debug)      declare -r OPT_DEBUG=1         ; shift   ;;
+    -b | --buffer)     declare -r ARG_BUFFER="$2"     ; shift 2 ;;
+    -c | --char)       declare -r ARG_CHAR="$2"       ; shift 2 ;;
+    -e | --expression) declare -r ARG_EXPRESSION="$3" ; shift 2 ;;
     # -- means the end of the arguments; drop this, and break out of the while loop
     --) shift; break ;;
     # If invalid options were passed, then getopt should have reported an error,
@@ -51,149 +79,109 @@ do
   esac
 done
 
-INFLILE="${1:-${DEV_NULL}}" && shift # play it save
-#INFLILE="${1}"&& shift #
-if [[ ${DEBUG} ]]; then 
+#if [[ ! -z ${@} ]]; then declare -r INFLILE=${1} ; fi # the remaining parameter is supposed to be the filname
+if [[ ! -z ${@} ]]; then INFLILE=${1} ; fi # the remaining parameter is supposed to be the filname
+
+if [[ ${OPT_DEBUG} ]]; then 
    echo "PARSED_ARGUMENTS is '$PARSED_ARGUMENTS'"
-   echo "DEBUG      : '${DEBUG}'"
-   echo "BUFFER     : '${BUFFER}'"
-   echo "CHAR       : '${CHAR}'"
-   echo "EXPRESSION : '${EXPRESSION}'"
+   echo "OPT_DEBUG      : '${OPT_DEBUG}'"
+   echo "ARG_BUFFER     : '${ARG_BUFFER}'"
+   echo "ARG_CHAR       : '${ARG_CHAR}'"
+   echo "ARG_EXPRESSION : '${ARG_EXPRESSION}'"
+   echo "INFILE         : '${INFLILE}' (alias filename)"
    echo "Parameters remaining are: '${@}'"
 fi
 
-if [[ ! -r ${INFLILE} ]]; then stop "file '${INFLILE}' not readable or does not exist" '22' ; fi
-from_source_file=${INFLILE}
+if [[ ! -r ${INFLILE} ]]; then stop "filename '${INFLILE}' is not readable or does not exist" '22' ; fi
+declare -r from_source_file=${INFLILE}; 
  
 ORIG_IFS="$IFS"
-IFS=$'|\n;' ; read -d '' -a  source_code  < ${from_source_file} # don't stop on 'pipes', 'newlines' and 'semicolons'
+IFS=$';\n'
+set +o errexit # -d '' works like a charm, but with exit code (1)?!
+read -d '' -a source_code < "${from_source_file}" # don't stop on 'pipes', 'newlines' and 'semicolons' 
+set -o errexit
 IFS="$ORIG_IFS"
-if [[ ${DEBUG} ]]; then echo '---';for element in "${source_code[@]}" ; do echo "$element" ; done ; echo '---'; fi
+if [[ ${OPT_DEBUG} ]]; then echo '---';for element in "${source_code[@]}" ; do echo "$element" ; done ; echo '---'; fi
 
-CHAR_AMPERSAND='&'
-CHAR_EOF=EOF
-CHAR_NEWLINE='\n'
-CHAR_CARRIAGE_RETURN='\r'
-CHAR_PIPE='|'
-CHAR_SEMICOLON=';'
-CHAR_COLON=':'
-CHAR_PLUS_SIGN='+'
-CHAR_MINUS_SIGN='-'
-CHAR_EQUAL_SIGN='='
-CHAR_COMMA=','
-CHAR_PARAN_OPEN='('
-CHAR_PARAN_CLOSE=')'
-CHAR_QUOTE="'"
-CHAR_DOUBLE_QUOTE='"'
-CHAR_CURLY_OPEN='{'
-CHAR_CURLY_CLOSE='}'
-CHAR_MUL='*'
-CHAR_DIV='/'
-CHAR_TAB='\t'
-PASS='0'
+# remove recursion, hence the use of global variable is sufficient
 
-buff=''
-c=''
-token=''
-
+################################################################################
+### start of function lex ######################################################
+################################################################################
 function lex () {
-
-buff="${1}"
-c="${buff:0:1}"
-token="${2}"
-
-#echo -n "${token} " >&3
-if [[ ! -z ${token} ]]; then echo "${token} " >&3 ; fi
-
-if [[ ${DEBUG} ]]; then
-   echo "token = »${token}«"
-   echo "c = »${c}«"              
-   echo "buff = »${buff}« [${#buff}]"
-fi
-
-if [[ 1 -gt "${#buff}" ]]; then return ; fi # here it's about time for the nest line of code
-if [[ 1 -gt "${#c}"    ]]; then return ; fi
-
-case "${c}" in
-   # Ignore whitespacce
-   [[:space:]]) 
-      ###"pass:  # >>${c}<< is a [line breaking] whitspace"
-      token=''
-      if [[ 2 -gt ${#buff} ]]; then buff=""; fi
-      buff="${buff: -(${#buff}-1)}"
-      if [[ 1 -gt ${#buff} ]]; then return; fi
-      lex "${buff}" "${token}"
-   ;;
-   # special character        
-   "${CHAR_PARAN_OPEN}"|"${CHAR_PARAN_CLOSE}"|"${CHAR_CURLY_OPEN}"|"${CHAR_CURLY_CLOSE}"|"${CHAR_COMMA}"|"${CHAR_SEMICOLON}"|"${CHAR_EQUAL_SIGN}"|"${CHAR_COLON}")
-      ###"yield(c, '') # is a special character "
-      token="('${c}', '')"
-      if [[ 2 -gt ${#buff} ]]; then buff=""; fi
-      buff="${buff: -(${#buff}-1)}"
-      lex "${buff}" "${token}"
-   ;;
-   # operator
-   "${CHAR_PLUS_SIGN}"|"${CHAR_MINUS_SIGN}"|"\${CHAR_MUL}"|"${CHAR_DIV}")
-      ###"yield ('operation', c) # operator >>${c}<< "
-      token="('operation', '${c}')"
-      if [[ 2 -gt ${#buff} ]]; then buff=""; fi
-      buff="${buff: -(${#buff}-1)}"
-      lex "${buff}" "${token}"
-   ;;
-   # string
-   "${CHAR_QUOTE}"|"${CHAR_DOUBLE_QUOTE}")
-      ###"yield ('string', _scan_string(c, chars)) # >>${c}<<"
-      pattern="([\ a-zA-Z0-9.:,;%?=&$§^#_\(\)\{\})\[\]]){0,}"
-      string_plus_quotes="$(grep -o -P "[${c}]${pattern}[${c}]" <<< ${buff})"
-      string_len_plus_quotes="${#string_plus_quotes}"
-      if [[ ${string_plus_quotes: -1} != ${c} ]]; then stop 'A string ran off the end of the program.' '77' ; fi
-      string=${string_plus_quotes:1:-1}
-      token="('string', '${string}')"
-      string_len="${#string}"
-      buff="${buff:${string_len_plus_quotes}:${#buff}}"
-      lex "${buff}" "${token}"
-   ;;
-   # number
-   [[:digit:]]|".")
-      ###"yield ('number', _scan(c, chars, '[.0-9]))# >>${c}<<"
-      sloppy=""
-      pattern="(^([[:digit:]])*([.][[:digit:]]){,1}([[:digit:]])*)"
-      number=$(grep -Eo "${pattern}" <<< ${buff})
-      if [[ ${number} == '' ]]; then stop "'$(grep -Eo "[-+0-9.]*" <<< ${buff})' is not a number" '1' ; fi
-      token="('number', '${number}')"
-      if [[ ${#number} -eq ${#buff} ]]; then buff=""; fi
-      buff="${buff:${#number}:${#buff}}"
-      lex "${buff}" "${token}"
-   ;;
-   # symbols
-   [[:alpha:]])
-      ###'yield ('symbol', _scan(c, chars, "[_a-zA-Z0-9]"))'
-      symbol=$(grep -o -E "^(([[:alpha:]]+)([[:alnum:][_])*)*" <<< ${buff})
-      token="('symbol', '${symbol}')"
-      if [[ ${#symbol} -eq ${#buff} ]]; then buff=""; fi
-      buff="${buff:${#symbol}:${#buff}}"
-      lex "${buff}" "${token}"
-   ;;
-   # TAB not allowed
-   "${CHAR_TAB}")
-      stop "raise Exception(\"Tabs >>${c}<< are not allowed in Cell\")" '1'
-      ;;
-   *)
-      stop "raise Exception(\"Unexpected character: >>${c}<<\")" '1'
-   ;;
-esac
+   declare buff="${1}"
+   declare token="${2}"
+   declare c
+   declare -i amount_of_processed_chars=0
+   while [[ true ]]; do
+      buff=${buff:${amount_of_processed_chars}:${#buff}}
+      c="${buff:0:1}"
+      if [[ ! -z ${token} ]]; then echo "${token} " >&3 ; fi
+      #############################
+      [[ 0 -lt ${#buff} ]] || break
+      #############################      
+      if [[ ${OPT_DEBUG} ]]; then
+         echo "token = »${token}«"
+         echo "c = »${c}«"              
+         echo "buff = »${buff}« [${#buff}]"
+         echo "amount_of_processed_chars = [${amount_of_processed_chars}]"
+      fi      
+      case "${c}" in
+         [[:space:]]) 
+            token="${_EMPTY_TOKEN_}"
+            amount_of_processed_chars=1
+         ;;
+         "${_PARAN_OPEN_}"|"${_PARAN_CLOSE_}"|"${_CURLY_OPEN_}"|"${_CURLY_CLOSE_}"|"${_COMMA_}"|"${_SEMICOLON_}"|"${_EQUAL_SIGN_}"|"${_COLON_}")
+            token="('${c}', '${_NO_VALUE_}')" # special character are thier own type, but without a value
+            amount_of_processed_chars=1
+         ;;
+         "${_PLUS_SIGN_}"|"${_MINUS_SIGN_}"|"\${_MUL_}"|"${_DIV_}")
+            token="('${_OPERATION_TOKEN_}', '${c}')"
+            amount_of_processed_chars=1
+         ;;
+         "${_QUOTE_}"|"${_DOUBLE_QUOTE_}")
+            pattern="([\ a-zA-Z0-9.:,;%?=&$§^#_\(\)\{\})\[\]]){0,}"
+            string_plus_quotes="$(grep -o -P "[${c}]${pattern}[${c}]" <<< ${buff})"
+            if [[ ${string_plus_quotes: -1} != ${c} ]]; then stop 'A string ran off the end of the program.' '77' ; fi
+            string=${string_plus_quotes:1:-1}
+            token="('${_STRING_TOKEN_}', '${string}')"
+            amount_of_processed_chars="${#string_plus_quotes}"
+         ;;
+         [[:digit:]]|".")
+            sloppy="[-+0-9.]*"
+            pattern="(^([[:digit:]])*([.][[:digit:]]){,1}([[:digit:]])*)"
+            number=$(grep -Eo "${pattern}" <<< ${buff})
+            if [[ ${number} == '' ]]; then stop "'$(grep -Eo "${sloppy}" <<< ${buff})' is not a number" '1' ; fi
+            token="('${_NUMBER_TOKEN_}', '${number}')"
+            amount_of_processed_chars="${#number}"
+         ;;
+         [[:alpha:]])
+            symbol=$(grep -o -E "^(([[:alpha:]]+)([[:alnum:][_])*)" <<< ${buff})
+            token="('${_SYMBOL_TOKEN_}', '${symbol}')"
+            amount_of_processed_chars="${#symbol}"
+         ;;
+         "${_TAB_}")
+            stop "Tabs are not allowed in Cell" '1'
+            ;;
+         *)
+            stop "Unexpected character: >>${c}<<" '1'
+         ;;
+      esac
+   done
 }
+################################################################################
+### end of function lex ########################################################
+################################################################################
 
-if [[ ${BUFFER} ]]; then
-   line=$(tr -d '\n\r\t' <<< ${BUFFER})
-   chars=${#line}
-      lex "${line}" "('BUFFER', '${line}')"
+if [[ ${ARG_BUFFER} ]]; then
+   declare buff_line="$(tr -d '\n\r\t' <<< ${ARG_BUFFER})" 
+   declare buff_line_token="('ARG_BUFFER', '${buff_line}')"
+   lex "${buff_line}" "${buff_line_token}"
 fi
 
 amount_of_lines=${#source_code[*]}
-if [[ amount_of_lines -gt 0 ]]; then
-   for (( line_no=0; line_no<$(( $amount_of_lines )); line_no++ )); do
-      line=${source_code[line_no]}
-      lex "${line}${CHAR_SEMICOLON}" "('LINE_${line_no}', '${line}${CHAR_SEMICOLON}')"
-   done
-fi
+for (( line_no=0; line_no<$(( $amount_of_lines )); line_no++ )); do
+   declare one_line="${source_code[line_no]}${_SEMICOLON_}" # the semicolon was stipprd while reading INFILE into the array
+   declare extra_line_token="('LINE_${line_no}', '${one_line}')" 
+   lex "${one_line}" "${extra_line_token}"
+done
