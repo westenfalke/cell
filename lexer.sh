e0ba84a2dad31921ed9b999ead3bdde3643f5d31
Stefan Welke <7465621+westenfalke@users.noreply.github.com>
2022-10-12 01:03:04 +0000

    removed recursion

--- lexer.sh
+++ lexer.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 exec 3>&1 1>&2
+set -o errexit
 set -o nounset
 
 declare -r _EMPTY_TOKEN_=''
@@ -103,95 +104,90 @@ IFS="$ORIG_IFS"
 if [[ ${OPT_DEBUG} ]]; then echo '---';for element in "${source_code[@]}" ; do echo "$element" ; done ; echo '---'; fi
 
 # remove recursion, hence the use of global variable is sufficient
-declare BUFF=''
-declare C=''
-declare TOKEN=''
-declare -i AMOUNT_OF_PROCESSED_CHARS=0
+
+################################################################################
+### start of function lex ######################################################
+################################################################################
 function lex () {
-
-BUFF="${1}"
-BUFF=${BUFF:${AMOUNT_OF_PROCESSED_CHARS}:${#BUFF}}
-C="${BUFF:0:1}"
-TOKEN="${2}"
-
-#echo -n "${TOKEN} " >&3
-if [[ ! -z ${TOKEN} ]]; then echo "${TOKEN} " >&3 ; fi
-
-if [[ ${OPT_DEBUG} ]]; then
-   echo "TOKEN = »${TOKEN}«"
-   echo "C = »${C}«"              
-   echo "BUFF = »${BUFF}« [${#BUFF}]"
-   echo "AMOUNT_OF_PROCESSED_CHARS = [${AMOUNT_OF_PROCESSED_CHARS}]"
-fi
-
-if [[ 1 -gt "${#BUFF}" ]]; then return ; fi # here it's about time for the nest line of code
-#if [[ 1 -gt "${#C}"    ]]; then return ; fi
-
-case "${C}" in
-   [[:space:]]) 
-      TOKEN="${_EMPTY_TOKEN_}"
-      if [[ 2 -gt ${#BUFF} ]]; then BUFF=""; fi
-      AMOUNT_OF_PROCESSED_CHARS=1
-      lex "${BUFF}" "${TOKEN}"
-   ;;
-   "${_PARAN_OPEN_}"|"${_PARAN_CLOSE_}"|"${_CURLY_OPEN_}"|"${_CURLY_CLOSE_}"|"${_COMMA_}"|"${_SEMICOLON_}"|"${_EQUAL_SIGN_}"|"${_COLON_}")
-      TOKEN="('${C}', '${_NO_VALUE_}')" # special character are thier own type, but without a value
-      if [[ 2 -gt ${#BUFF} ]]; then BUFF=""; fi
-      AMOUNT_OF_PROCESSED_CHARS=1
-      lex "${BUFF}" "${TOKEN}"
-   ;;
-   "${_PLUS_SIGN_}"|"${_MINUS_SIGN_}"|"\${_MUL_}"|"${_DIV_}")
-      TOKEN="('${_OPERATION_TOKEN_}', '${C}')"
-      if [[ 2 -gt ${#BUFF} ]]; then BUFF=""; fi
-      AMOUNT_OF_PROCESSED_CHARS=1
-      lex "${BUFF}" "${TOKEN}"
-   ;;
-   "${_QUOTE_}"|"${_DOUBLE_QUOTE_}")
-      pattern="([\ a-zA-Z0-9.:,;%?=&$§^#_\(\)\{\})\[\]]){0,}"
-      string_plus_quotes="$(grep -o -P "[${C}]${pattern}[${C}]" <<< ${BUFF})"
-      string_len_plus_quotes="${#string_plus_quotes}"
-      if [[ ${string_plus_quotes: -1} != ${C} ]]; then stop 'A string ran off the end of the program.' '77' ; fi
-      string=${string_plus_quotes:1:-1}
-      TOKEN="('${_STRING_TOKEN_}', '${string}')"
-      string_len="${#string}"
-      AMOUNT_OF_PROCESSED_CHARS="${#string_plus_quotes}"
-      lex "${BUFF}" "${TOKEN}"
-   ;;
-   [[:digit:]]|".")
-      sloppy="[-+0-9.]*"
-      pattern="(^([[:digit:]])*([.][[:digit:]]){,1}([[:digit:]])*)"
-      number=$(grep -Eo "${pattern}" <<< ${BUFF})
-      if [[ ${number} == '' ]]; then stop "'$(grep -Eo "${sloppy}" <<< ${BUFF})' is not a number" '1' ; fi
-      TOKEN="('${_NUMBER_TOKEN_}', '${number}')"
-      if [[ ${#number} -eq ${#BUFF} ]]; then BUFF=""; fi
-      AMOUNT_OF_PROCESSED_CHARS="${#number}"
-      lex "${BUFF}" "${TOKEN}"
-   ;;
-   [[:alpha:]])
-      symbol=$(grep -o -E "^(([[:alpha:]]+)([[:alnum:][_])*)" <<< ${BUFF})
-      TOKEN="('${_SYMBOL_TOKEN_}', '${symbol}')"
-      #if [[ ${#symbol} -eq ${#BUFF} ]]; then BUFF=""; fi
-      AMOUNT_OF_PROCESSED_CHARS="${#symbol}"
-      lex "${BUFF}" "${TOKEN}"
-   ;;
-   "${_TAB_}")
-      stop "Tabs are not allowed in Cell" '1'
-      ;;
-   *)
-      stop "Unexpected character: >>${C}<<" '1'
-   ;;
-esac
+   declare BUFF="${1}"
+   declare TOKEN="${2}"
+   declare C
+   declare -i AMOUNT_OF_PROCESSED_CHARS=0
+   while [[ true ]]; do
+      BUFF=${BUFF:${AMOUNT_OF_PROCESSED_CHARS}:${#BUFF}}
+      C="${BUFF:0:1}"
+      if [[ ! -z ${TOKEN} ]]; then echo "${TOKEN} " >&3 ; fi
+      #############################
+      [[ 0 -lt ${#BUFF} ]] || break
+      #############################      
+      if [[ ${OPT_DEBUG} ]]; then
+         echo "TOKEN = »${TOKEN}«"
+         echo "C = »${C}«"              
+         echo "BUFF = »${BUFF}« [${#BUFF}]"
+         echo "AMOUNT_OF_PROCESSED_CHARS = [${AMOUNT_OF_PROCESSED_CHARS}]"
+      fi      
+      case "${C}" in
+         [[:space:]]) 
+            TOKEN="${_EMPTY_TOKEN_}"
+            if [[ 2 -gt ${#BUFF} ]]; then BUFF=""; fi
+            AMOUNT_OF_PROCESSED_CHARS=1
+         ;;
+         "${_PARAN_OPEN_}"|"${_PARAN_CLOSE_}"|"${_CURLY_OPEN_}"|"${_CURLY_CLOSE_}"|"${_COMMA_}"|"${_SEMICOLON_}"|"${_EQUAL_SIGN_}"|"${_COLON_}")
+            TOKEN="('${C}', '${_NO_VALUE_}')" # special character are thier own type, but without a value
+            if [[ 2 -gt ${#BUFF} ]]; then BUFF=""; fi
+            AMOUNT_OF_PROCESSED_CHARS=1
+         ;;
+         "${_PLUS_SIGN_}"|"${_MINUS_SIGN_}"|"\${_MUL_}"|"${_DIV_}")
+            TOKEN="('${_OPERATION_TOKEN_}', '${C}')"
+            if [[ 2 -gt ${#BUFF} ]]; then BUFF=""; fi
+            AMOUNT_OF_PROCESSED_CHARS=1
+         ;;
+         "${_QUOTE_}"|"${_DOUBLE_QUOTE_}")
+            pattern="([\ a-zA-Z0-9.:,;%?=&$§^#_\(\)\{\})\[\]]){0,}"
+            string_plus_quotes="$(grep -o -P "[${C}]${pattern}[${C}]" <<< ${BUFF})"
+            string_len_plus_quotes="${#string_plus_quotes}"
+            if [[ ${string_plus_quotes: -1} != ${C} ]]; then stop 'A string ran off the end of the program.' '77' ; fi
+            string=${string_plus_quotes:1:-1}
+            TOKEN="('${_STRING_TOKEN_}', '${string}')"
+            string_len="${#string}"
+            AMOUNT_OF_PROCESSED_CHARS="${#string_plus_quotes}"
+         ;;
+         [[:digit:]]|".")
+            sloppy="[-+0-9.]*"
+            pattern="(^([[:digit:]])*([.][[:digit:]]){,1}([[:digit:]])*)"
+            number=$(grep -Eo "${pattern}" <<< ${BUFF})
+            if [[ ${number} == '' ]]; then stop "'$(grep -Eo "${sloppy}" <<< ${BUFF})' is not a number" '1' ; fi
+            TOKEN="('${_NUMBER_TOKEN_}', '${number}')"
+            #if [[ ${#number} -eq ${#BUFF} ]]; then BUFF=""; fi
+            AMOUNT_OF_PROCESSED_CHARS="${#number}"
+         ;;
+         [[:alpha:]])
+            symbol=$(grep -o -E "^(([[:alpha:]]+)([[:alnum:][_])*)" <<< ${BUFF})
+            TOKEN="('${_SYMBOL_TOKEN_}', '${symbol}')"
+            AMOUNT_OF_PROCESSED_CHARS="${#symbol}"
+         ;;
+         "${_TAB_}")
+            stop "Tabs are not allowed in Cell" '1'
+            ;;
+         *)
+            stop "Unexpected character: >>${C}<<" '1'
+         ;;
+      esac
+   done
 }
+################################################################################
+### end of function lex ########################################################
+################################################################################
 
 if [[ ${ARG_BUFFER} ]]; then
-   line=$(tr -d '\n\r\t' <<< ${ARG_BUFFER})
-   chars=${#line}
-      lex "${line}" "('ARG_BUFFER', '${line}')"
+   declare buff_line="$(tr -d '\n\r\t' <<< ${ARG_BUFFER})" 
+   declare buff_line_token="('ARG_BUFFER', '${buff_line}')"
+   lex "${buff_line}" "${buff_line_token}"
 fi
 
 amount_of_lines=${#source_code[*]}
 for (( line_no=0; line_no<$(( $amount_of_lines )); line_no++ )); do
-   AMOUNT_OF_PROCESSED_CHARS=0 # reset
-   line=${source_code[line_no]}
-   lex "${line}${_SEMICOLON_}" "('LINE_${line_no}', '${line}${_SEMICOLON_}')"
+   declare one_line="${source_code[line_no]}${_SEMICOLON_}" # the semicolon was stipprd while reading INFILE into the array
+   declare extra_line_token="('LINE_${line_no}', '${one_line}')" 
+   lex "${one_line}" "${extra_line_token}"
 done
